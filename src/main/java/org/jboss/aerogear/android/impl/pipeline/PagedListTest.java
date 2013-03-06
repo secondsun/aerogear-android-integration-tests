57a773ac8f06a3a6969ae59c7ad43a4056d3a7c7
Summers Pittman <supittma@redhat.com>
2013-03-06 10:37:51 -0500

    Added RestAdapter Tests

---
+++ src/main/java/org/jboss/aerogear/android/impl/pipeline/PagedListTest.java
@@ -0,0 +1,49 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.android.impl.pipeline;
+
+import android.test.AndroidTestCase;
+import org.jboss.aerogear.android.impl.pipeline.paging.WrappingPagedList;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import org.jboss.aerogear.android.Callback;
+import org.jboss.aerogear.android.ReadFilter;
+import org.jboss.aerogear.android.pipeline.Pipe;
+import static org.mockito.Mockito.*;
+
+public class PagedListTest extends AndroidTestCase {
+
+    public void testNext() {
+        Pipe pipe = mock(Pipe.class);
+        ReadFilter next = new ReadFilter();
+        List delegate = new ArrayList();
+        ReadFilter previous = new ReadFilter();
+
+        next.setLinkUri(URI.create("./next"));
+        previous.setLinkUri(URI.create("./previous"));
+
+        WrappingPagedList list = new WrappingPagedList(pipe, delegate, next, previous);
+        list.next(mock(Callback.class));
+        list.previous(mock(Callback.class));
+
+        verify(pipe).readWithFilter(eq(next), any(Callback.class));
+        verify(pipe).readWithFilter(eq(previous), any(Callback.class));
+
+    }
+
+}
