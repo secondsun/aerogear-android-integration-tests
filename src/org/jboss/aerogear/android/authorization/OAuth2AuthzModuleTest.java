8681f07d0b258ab3948e6fea8e641e8c6da336b0
Summers Pittman <secondsun@gmail.com>
2014-05-14 11:01:43 -0400

    adding getAuthz tokens test

--- src/org/jboss/aerogear/android/authorization/OAuth2AuthzModuleTest.java
+++ src/org/jboss/aerogear/android/authorization/OAuth2AuthzModuleTest.java
@@ -1,22 +1,21 @@
 package org.jboss.aerogear.android.authorization;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.util.Pair;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.jboss.aerogear.MainActivity;
 import org.jboss.aerogear.android.Callback;
+import org.jboss.aerogear.android.authentication.AuthorizationFields;
 import org.jboss.aerogear.android.impl.authz.AuthzConfig;
 import org.jboss.aerogear.android.impl.authz.oauth2.OAuth2AuthzModule;
+import org.jboss.aerogear.android.impl.authz.oauth2.OAuth2AuthzSession;
+import org.jboss.aerogear.android.impl.helper.UnitTestUtils;
 import org.jboss.aerogear.android.impl.util.PatchedActivityInstrumentationTestCase;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.any;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
@@ -57,5 +56,19 @@ public class OAuth2AuthzModuleTest extends PatchedActivityInstrumentationTestCas
         
         Mockito.verify(mockActivity, times(1)).bindService((Intent) any(),(ServiceConnection) any(), any(Integer.class));
         
+    }
+    
+    public void testGetAccessTokens() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        
+        AuthzConfig config = new AuthzConfig(BASE_URL, "name");
+        OAuth2AuthzModule module = new OAuth2AuthzModule(config);
+        
+        OAuth2AuthzSession account = new OAuth2AuthzSession();
+        account.setAccessToken("testToken");
+        
+        UnitTestUtils.setPrivateField(module, "account", account);
+        
+        assertEquals("Bearer testToken", module.getAuthorizationFields(null, null, null).getHeaders().get(0).second);
+        assertEquals("Authorization", module.getAuthorizationFields(null, null, null).getHeaders().get(0).first);
     }
 }
