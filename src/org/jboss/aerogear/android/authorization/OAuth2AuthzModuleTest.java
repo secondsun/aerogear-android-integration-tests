7b02779820cb35d606fbaf76fcc17258a1ab1cb6
Summers Pittman <secondsun@gmail.com>
2014-05-14 11:54:25 -0400

    OAuth2 Authz Module access token test

--- src/org/jboss/aerogear/android/authorization/OAuth2AuthzModuleTest.java
+++ src/org/jboss/aerogear/android/authorization/OAuth2AuthzModuleTest.java
@@ -4,17 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.util.Pair;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.jboss.aerogear.MainActivity;
 import org.jboss.aerogear.android.Callback;
 import org.jboss.aerogear.android.authentication.AuthorizationFields;
 import org.jboss.aerogear.android.impl.authz.AuthzConfig;
+import org.jboss.aerogear.android.impl.authz.AuthzService;
 import org.jboss.aerogear.android.impl.authz.oauth2.OAuth2AuthzModule;
 import org.jboss.aerogear.android.impl.authz.oauth2.OAuth2AuthzSession;
 import org.jboss.aerogear.android.impl.helper.UnitTestUtils;
 import org.jboss.aerogear.android.impl.util.PatchedActivityInstrumentationTestCase;
+import org.jboss.aerogear.android.impl.util.VoidCallback;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.matches;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -70,5 +77,33 @@ public class OAuth2AuthzModuleTest extends PatchedActivityInstrumentationTestCas
         
         assertEquals("Bearer testToken", module.getAuthorizationFields(null, null, null).getHeaders().get(0).second);
         assertEquals("Authorization", module.getAuthorizationFields(null, null, null).getHeaders().get(0).first);
+    }
+    
+    public void testOAuth2AccessCallback() throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchFieldException {
+        AuthzService mockService = mock(AuthzService.class);
+        Activity mockActivity = mock(Activity.class);
+        ServiceConnection mockServiceConnection = mock(ServiceConnection.class);
+        OAuth2AuthzSession account = new OAuth2AuthzSession();
+        account.setAccessToken("testToken");
+        
+        when(mockService.getAccount(matches("testAccountId"))).thenReturn(account);
+        
+        AuthzConfig config = new AuthzConfig(BASE_URL, "name");
+        config.setAccountId("testAccountId");
+        
+        OAuth2AuthzModule module = new OAuth2AuthzModule(config);
+        Class<?> callbackClass = Class.forName("org.jboss.aerogear.android.impl.authz.oauth2.OAuth2AuthzModule$OAuth2AccessCallback");
+        Constructor<?> constructor = callbackClass.getDeclaredConstructor(OAuth2AuthzModule.class, Activity.class, Callback.class, ServiceConnection.class);
+        constructor.setAccessible(true);
+        
+        Callback callback = (Callback) constructor.newInstance(module, mockActivity, new VoidCallback(), mockServiceConnection);
+        
+        
+        UnitTestUtils.setPrivateField(module, "service", mockService);
+        
+        callback.onSuccess("testToken");
+        
+        Mockito.verify(mockActivity, times(1)).unbindService(eq(mockServiceConnection));
+        assertEquals("testToken", UnitTestUtils.getPrivateField(module, "account", OAuth2AuthzSession.class).getAccessToken());
     }
 }
